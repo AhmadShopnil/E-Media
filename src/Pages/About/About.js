72c4b1d65b85b17715bd9fd102532f862c9519cc
AhmadShopnil <ahmadshopnil75@gmail.com>
2022-12-28 00:35:55 +0600

    setup about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const About = () => {
+
+    const handleEditProfile = (event) => {
+        event.preventDeafault()
+        const form = event.target;
+        const name = event.name.value
+        const address = event.address.value
+        const university = event.university.value
+    }
+
+
+
     return (
-        <div>
-            <h2>About</h2>
+        <div className=' w-full'>
+            <div className='border-2 w-1/2 mx-auto p-10'>
+                <img className="mask mask-circle" src="https://placeimg.com/160/160/arch" />
+                <form onSubmit={handleEditProfile} className='grid grid-cols-1 gap-4 my-4'>
+                    <input name='name' type="text" defaultValue={'Shopnil'} placeholder="Name" className="input input-bordered input-primary w-full max-w-xs" />
+                    <input name='address' type="text" defaultValue={'Tangail'} placeholder="Adress" className="input input-bordered input-primary w-full max-w-xs" />
+                    <input name='university' type="text" defaultValue={'Govt Saadat College'} placeholder="University" className="input input-bordered input-primary w-full max-w-xs" />
+                    <input className='btn ' type="submit" value="Save Changes" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/PostDetails/PostDetails.js
+++ src/Pages/PostDetails/PostDetails.js
@@ -2,16 +2,52 @@ import React from 'react';
 import AddComment from '../../Component/AddComment/AddComment';
 
 const PostDetails = () => {
+
+    const comments = [
+        {
+            userName: 'Shopnil',
+            text: 'Wow asd d asd  asd dad sad sad asd asds fa sadsa sd sd'
+        },
+        {
+            userName: 'Ahamad',
+            text: 'Good post keep it up'
+        },
+        {
+            userName: 'Dip',
+            text: 'Nice'
+        },
+    ]
+
     return (
-        <div>
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLLUBkJgwdSnMEx7TGzJ5p_2kz8JxCGv1bW3EKuBCa2w&s" alt="" />
+        <div className='mx-20'>
+            <div className='w-full'>
+                <img className='w-3/4 mx-auto h-96 ' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLLUBkJgwdSnMEx7TGzJ5p_2kz8JxCGv1bW3EKuBCa2w&s" alt="" />
                 <div>
-                    <h3>Title</h3>
+                    <h3 className=' font-bold my-4'>Author : <span className='text-2xl font-light'>Shopnil</span></h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus nisi expedita eum autem dignissimos cupiditate saepe modi quidem sequi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus nisi expedita eum autem dignissimos cupiditate saepe modi quidem sequi?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptatibus nisi expedita eum autem dignissimos cupiditate saepe modi quidem sequi?</p>
                 </div>
             </div>
-            <AddComment></AddComment>
+
+
+            <div className='my-8'>
+                <AddComment></AddComment>
+                <h2 className='font-semibold text-xl mb-4'>All Comments--</h2>
+                <div className='flex flex-col gap-4 md:ml-10 '>
+                    {
+                        comments.map(comment => {
+                            return <div className='flex gap-1'>
+                                <img className=" w-10 mask mask-circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1cDnT1Q5ZrkfLfxiSgFvC2ZsjpngynJGvg&usqp=CAU" />
+                                <div className='bg-blue-50 p-3 rounded-xl'>
+                                    <h4 className='font-semibold'>{comment?.userName}</h4>
+                                    <small>{comment?.text}</small>
+
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+
+            </div>
         </div>
     );
 };
