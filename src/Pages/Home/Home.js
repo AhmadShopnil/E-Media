20fd44ce2b73cf8febfe2f5926d8a9d372ceb059
AhmadShopnil <ahmadshopnil75@gmail.com>
2022-12-30 19:49:28 +0600

    change homepage design

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import LeftDrawer from '../../Component/Drawer/LeftDrawer';
+import { AuthContext } from '../../context/AuthProvider';
 import NewsFeed from './NewsFeed/NewsFeed';
 import PostArea from './PostArea/PostArea';
 
 const Home = () => {
+    const { user } = useContext(AuthContext)
+
+
     return (
         <div className='flex min-h-screen'>
 
@@ -15,7 +20,19 @@ const Home = () => {
                     <h4 className=' mt-16 text-center font-semibold text-2xl'> Coming Soon</h4>
                 </div>
                 <div className='col-span-4 md:col-span-2  grid grid-cols-1 justify-center'>
-                    <PostArea></PostArea>
+                    {
+                        user?.uid ?
+                            <>
+                                <PostArea></PostArea>
+                            </>
+                            :
+                            <>
+                                <span className='text-xl text-center font-bold mb-4'>Please <Link className='btn btn-accent btn-xs' to='/login'>Login</Link> First To Create a Post</span>
+
+                            </>
+                    }
+
+
                     <NewsFeed></NewsFeed>
                 </div>
                 <div className=' bg-green-50 hidden md:block  '>
